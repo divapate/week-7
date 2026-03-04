6752a1b7a1cca7592fa3527d8bf6df5ed2a67a6f
Hamza Almani <github-halmani@iu.edu>
2026-03-04 17:20:57 -0500

    libraries installed and missing geolocator added

--- loader.py
+++ loader.py
@@ -1,41 +1,70 @@
-'''
-Script to load geographical data into a pandas DataFrame, and save it as a CSV file.
-'''
+"""
+Script to load geographical data into a pandas DataFrame
+and save it as a CSV file.
+"""
 
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderServiceError
 import pandas as pd
 
 
-def get_geolocator(agent='h501-student'):
+def get_geolocator(agent: str = "h501-student") -> Nominatim:
     """
-    Initiate a Nominatim geolocator instance given an `agent`.
-
-    Parameters
-    ----------
-    agent : str, optional
-        Agent name for Nominatim, by default 'h501-student'
+    Create and return a Nominatim geolocator instance.
     """
     return Nominatim(user_agent=agent)
 
-def fetch_location_data(geolocator, loc):
-    location = geolocator.geocode(loc)
 
-    if location is None:
+def fetch_location_data(geo, location_name: str):
+    """
+    Fetch latitude, longitude, and type for a given location.
+    """
+    try:
+        location = geo.geocode(location_name)
+
+        if location is None:
+            return None
+
+        return {
+            "location": location_name,
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "type": getattr(location, "geo_type", None),
+        }
+
+    except GeocoderServiceError:
         return None
-    
-    return {"location": loc, "latitude": location.latitude, "longitude": location.longitude, "type": location.geo_type}
+    except Exception:
+        return None
 
-def build_geo_dataframe(locations):
-    geo_data = [fetch_location_data(geolocator, loc) for loc in locations]
-    
+
+def build_geo_dataframe(geo, locations):
+    """
+    Build a pandas DataFrame from a list of location names.
+    """
+    geo_data = []
+
+    for loc in locations:
+        data = fetch_location_data(geo, loc)
+        if data is not None:
+            geo_data.append(data)
+
     return pd.DataFrame(geo_data)
 
 
 if __name__ == "__main__":
-    geo = get_geolocator()
+    geolocator_instance = get_geolocator()
 
-    locations = ["Museum of Modern Art", "iuyt8765(*&)", "Alaska", "Franklin's Barbecue", "Burj Khalifa"]
+    locations = [
+        "Museum of Modern Art",
+        "iuyt8765(*&)",
+        "Alaska",
+        "Franklin's Barbecue",
+        "Burj Khalifa",
+    ]
 
-    df = build_geo_dataframe(locations)
+    df = build_geo_dataframe(geolocator_instance, locations)
 
-    df.to_csv("./geo_data.csv")
+    df.to_csv("./geo_data.csv", index=False)
+
+    print("Geo data saved to geo_data.csv")
